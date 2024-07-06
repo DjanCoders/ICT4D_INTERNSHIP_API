48572efe8ebab9bf0ea684c9c073f9ba90cb3774
Elianamos <elianamos29@gmail.com>
2024-07-06 19:31:25 +0300

    add the questionsviewset and configure media file handling

--- internship/urls.py
+++ internship/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -14,3 +16,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- internships/urls.py
+++ internships/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import InternshipViewSet, ApplicationViewSet
+from .views import InternshipViewSet, ApplicationViewSet, QuestionViewSet
 
 router = DefaultRouter()
 router.register('internships', InternshipViewSet)
 router.register('applications', ApplicationViewSet)
+router.register('questions', QuestionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
