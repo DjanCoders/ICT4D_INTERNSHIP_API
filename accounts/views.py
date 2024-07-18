09d9e82f4beca02f58b2fd041e7539497252902f
Elianamos <elianamos29@gmail.com>
2024-07-18 15:11:10 +0300

    update logout view

--- accounts/views.py
+++ accounts/views.py
@@ -36,5 +36,11 @@ class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
