93b9b6a0848f647a620c47447a08bfec9ce700b6
Elianamos <elianamos29@gmail.com>
2024-10-10 19:05:41 +0300

    update registration serializer: add username field

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,13 +5,14 @@ from .models import CustomUser, Profile
 class RegisterationSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('email', 'password', 'first_name', 'last_name')
+        fields = ('email', 'password', 'username' 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
             password=validated_data['password'],
+            username=validated_data['username'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name']
         )
